884f1d36e86331b8658149fea3ce9e355400a277
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-06-21 11:32:25 +0300

    1. Adding ingrediants to the shopping list 2. Deleting recipes

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Recipe } from '../../models/recipe';
 import { EditRecipePage } from '../edit-recipe/edit-recipe';
+import { ShoppingListService } from '../../services/shopping-list';
+import { RecipesService } from '../../services/recipes';
 
 @IonicPage()
 @Component({
@@ -14,14 +16,25 @@ export class RecipePage implements OnInit {
   index: number;
 
   constructor(private navCtrl: NavController,
-              private navParams: NavParams) {}
+              private navParams: NavParams,
+              private shoppingListService: ShoppingListService,
+              private recipesService: RecipesService) {}
 
   ngOnInit () {
     this.recipe = this.navParams.get('recipe');
     this.index = this.navParams.get('index');
   }
 
+  onAddIngrediantsToShoppingList() {
+    this.shoppingListService.addItems(this.recipe.ingrediants);
+  }
+
   onEditRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'Edit', recipe: this.recipe, index: this.index});
+  }
+
+  onDeleteRecipe() {
+    this.recipesService.removeRecipe(this.index);
+    this.navCtrl.popToRoot();
   }
 }
